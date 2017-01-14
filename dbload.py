f8047bd335ccf3f0ee2f84b55eb1c63de32a50de
Cynthia Shlezar <cynthiashlezar@email.arizona.edu>
2017-01-14 13:39:42 -0700

    reading specific columns from the csv

--- dbload.py
+++ dbload.py
@@ -1,5 +1,10 @@
 import csv
 with open('hatespeech.csv', newline='', encoding='utf-8', errors='ignore') as f:
     reader = csv.reader(f)
+
+    #included_cols specifies which column numbers we want to have access to
+    included_cols = [1, 2, 6, 7]
+
     for row in reader:
+        row = list(row[i] for i in included_cols)
         print(row)
