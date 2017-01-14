42b3a6575363fd94070074d6133aaf34a41077a6
Cynthia Shlezar <cynthiashlezar@email.arizona.edu>
2017-01-14 13:34:26 -0700

    reading from the csv

--- dbload.py
+++ dbload.py
@@ -1,5 +1,5 @@
 import csv
-with open('hatespeech.csv', newline='') as f:
+with open('hatespeech.csv', newline='', encoding='utf-8', errors='ignore') as f:
     reader = csv.reader(f)
     for row in reader:
         print(row)
